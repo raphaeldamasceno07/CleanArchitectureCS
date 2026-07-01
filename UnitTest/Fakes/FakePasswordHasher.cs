e51452662ef02fb641c082f16f0b3eccaa81315b
Developer <user@example.com>
2026-07-01 20:47:11 -0300

    test: add unit tests for application layer

---
+++ UnitTest/Fakes/FakePasswordHasher.cs
@@ -0,0 +1,16 @@
+﻿using Application.Interfaces;
+
+namespace UnitTest.Fakes;
+
+public class FakePasswordHasher : IPasswordHasher
+{
+    public string Hash(string password)
+    {
+        return $"{password}_hashed";
+    }
+
+    public bool Verify(string password, string hash)
+    {
+        return $"{password}_hashed" == hash;
+    }
+}
