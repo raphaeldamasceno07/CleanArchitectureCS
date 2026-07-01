4ba83e96e52885e420c179b674d7f6d854fe25dc
Developer <user@example.com>
2026-07-01 20:47:01 -0300

    feat(infrastructure): add database context and implementations

---
+++ Infrastructure/Repositories/InMemoryUserRepository.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using System.Collections.Concurrent;
+
+namespace Infrastructure.Repositories;
+
+public class InMemoryUserRepository : IUserRepository
+{
+    private readonly ConcurrentDictionary<Guid, User> _users = new();
+
+    public Task AddAsync(User user)
+    {
+        _users[user.Id] = user;
+        return Task.CompletedTask;
+    }
+
+    public Task<User?> GetByEmailAsync(string email)
+    {
+        var user = _users.Values.FirstOrDefault(u => u.Email.Equals(email, StringComparison.OrdinalIgnoreCase));
+
+        return Task.FromResult<User?>(user);
+    }
+
+    public Task<User?> GetByUserByIdAsync(Guid id)
+    {
+        _users.TryGetValue(id, out var user);
+        return Task.FromResult<User?>(user);
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        _users[user.Id] = user;
+        return Task.CompletedTask;
+    }
+}
