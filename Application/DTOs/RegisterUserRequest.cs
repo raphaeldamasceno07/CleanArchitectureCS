065b8fb128fdc52836682c862bed2ebb41aa518c
Developer <user@example.com>
2026-07-01 20:46:53 -0300

    feat(application): add application services and use cases

---
+++ Application/DTOs/RegisterUserRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs;
+
+public class RegisterUserRequest
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string? ProfilePhoto { get; set; }
+}
---
+++ Application/DTOs/RegisterUserResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.DTOs;
+
+public record RegisterUserResponse(
+  Guid Id,
+  string Name,
+  string Email,
+  string? ProfilePhoto
+);
