7d76f99633999a097650ddb279c30dad57d12bad
Developer <user@example.com>
2026-07-01 21:41:01 -0300

    feat(infra): setup entity framework core and appdbcontext

---
+++ Infrastructure/Persistance/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistance;
+
+public class AppDbContext:DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    // public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) {
+        base.OnModelCreating(modelBuilder);
+    }
+}
