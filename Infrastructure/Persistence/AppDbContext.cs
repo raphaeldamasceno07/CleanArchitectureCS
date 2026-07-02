0015702284c47086c4c3f184c7fbe62f9016ebea
Developer <user@example.com>
2026-07-02 10:36:07 -0300

    feat(infra): update database schema with initial user migration

--- Infrastructure/Persistence/AppDbContext.cs
+++ Infrastructure/Persistence/AppDbContext.cs
@@ -8,8 +8,6 @@ public class AppDbContext:DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-    public DbSet<User> Users { get; set; }
-
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
