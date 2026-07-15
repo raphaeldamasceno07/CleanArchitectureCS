59e09bc5fe146042f1dcf5ffa9b67eedd594a04a
Developer <user@example.com>
2026-07-15 10:14:55 -0300

    refactor: program

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,31 +1,69 @@
 using API.Middleware;
+using Application.UseCases;
+using Application.Validators.Customer;
 using Domain.Interfaces;
 using Infrastructure.Persistance;
 using Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
+using FluentValidation;
 using Scalar.AspNetCore;
 
-var bcilder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-bcilder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(bcilder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-bcilder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 
-bcilder.Services.AddControllers();
+builder.Services.AddScoped<IPasswordHasher, Infrastructure.Security.PasswordHasher>();
 
-bcilder.Services.AddOpenApi();
+builder.Services.AddScoped<RegisterCustomerUseCase>();
 
-var app = bcilder.Build();
+builder.Services.AddValidatorsFromAssemblyContaining<RegisterCustomerValidator>();
+
+builder.Services.AddControllers();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, cancellationToken) =>
+    {
+        document.Info.Title = "Clean Arch API";
+        return Task.CompletedTask;
+    });
+});
+
+var app = builder.Build();
 
 app.UseMiddleware<GlobalExceptionHandler>();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
+
+    app.MapScalarApiReference(options =>
+    {
+        options
+            .WithTitle("Clean Arch API")
+            .WithTheme(ScalarTheme.Kepler)
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient)
+            .WithOpenApiRoutePattern("/openapi/v1.json");
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
